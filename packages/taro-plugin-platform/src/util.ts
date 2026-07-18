974ea8733cbc7f48f731bca13122364154575ff6
dgz <dagouzhi@qq.com>
2026-07-18 19:36:53 +0800

    fix

--- packages/taro-plugin-platform/src/util.ts
+++ packages/taro-plugin-platform/src/util.ts
@@ -1,13 +1,7 @@
-import { chalk, isWindows } from '@tarojs/helper'
+import { chalk } from '@tarojs/helper'
 
 export function printDevelopmentTip (platform: string) {
   if (process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'test') {
-    let exampleCommand
-    if (isWindows) {
-      exampleCommand = `$ set NODE_ENV=production && taro build --type ${platform} --watch`
-    } else {
-      exampleCommand = `$ NODE_ENV=production taro build --type ${platform} --watch`
-    }
     console.log(chalk.yellowBright(`Tips: [${process.env.TARO_ENV}]`))
   }
 }
